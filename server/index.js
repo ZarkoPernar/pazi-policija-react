7a8c28b1588915891c0742a7d3264d07ec367b36
zarkopernar <zarko.pernar@gmail.com>
2016-12-23 23:17:13 +0100

    try again

--- server/index.js
+++ server/index.js
@@ -9,11 +9,15 @@ const PUBLIC_DIR = 'public'
 const routes = require('./routes')
 const api = require('./api')
 
+app.set('port', (process.env.PORT || 5000))
+
 app.use(express.static(PUBLIC_DIR))
 app.use(bodyParser.json())
 // app.use(checkUserAgent)
 
-app.listen(3000, function () {})
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port: ' + app.get('port'))
+})
 
 app.post(routes.NEAR, api.getNearbyLocations)
 app.post(routes.ADD, api.addLocation)
